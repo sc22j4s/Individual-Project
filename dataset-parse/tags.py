515eebdaa328abbaac660eba4b6bb78440a17bc2
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2024-12-08 21:59:04 +0000

    cad + pyclarity environment setup + some music parsing

---
+++ dataset-parse/tags.py
@@ -0,0 +1,20 @@
+
+
+def get_tag_list():
+
+    tags = {
+        "female-vocals": {}
+        "male-vocals": {}
+        "pop": {}
+        "rock": {}
+        "happy": {}
+        "sad": {}
+    } # type shit
+
+    """
+    TODO
+    Can get more quantitaitive info using pyAudioAnalysis
+    """
+
+
+    return tags
